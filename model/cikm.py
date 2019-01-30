cd0721543f0f47abe8d3280cbd79ef8417df956e
Ahmad Farag <ahmad.k.farag@gmail.com>
2019-01-30 16:05:58 -0500

    select from source regions for random walks create 'identity' sequence for regions we dont see in our walk fixed tokenzation bug

--- model/cikm.py
+++ model/cikm.py
@@ -31,9 +31,10 @@ def random_walk_distance(m, high, n_walks=10000, walk_length=8):
             p = [d / s for d in distances]
             # pick node to walk to
             walk = numpy.random.choice(list(range(0, len(distances))), size=1, p=p)[0]
-            walk_sequence.append(walk)
             # set this vertex to walk from
             v = walk
+            walk_sequence.append(f"{v}")
+        print(walk_sequence)
         walks.append(walk_sequence)
     return walks
 
@@ -47,16 +48,21 @@ def random_walk_time(m, source_v, n_walks=10000, walk_length=8):
         walk_sequence = []
         for i in range(0, walk_length):
             print(f"Walk {walk_num} - Step {i}")
+            print(f"Walking from {v}")
             # start walk
             # total counts for each time bucket
             sums = m[v].sum(axis=1)
             s = sums.sum()
-            # create probabilities
-            p = [x / s for x in sums]
-            walk = numpy.random.choice(m[v].shape[0], size=1, p=p)[0]
-            walk_sequence.append(walk)
-            # set this vertex to walk from
-            v = walk
+            if v in sources:
+                # create probabilities
+                p = [x / s for x in sums]
+                walk = numpy.random.choice(m[v].shape[0], size=1, p=p)[0]
+                # set this vertex to walk from
+                v = walk
+                walk_sequence.append(f"{v}")
+            else:
+                walk_sequence.append(f"{v}")
+        print(walk_sequence)
         walks.append(walk_sequence)
     return walks
 
@@ -76,25 +82,28 @@ def e_distance(d):
 
 distance_matrix = region_grid.get_distance_mtx(transform=e_distance)
 
-W, t, sources, ends = region_grid.create_flow_matrix(c['raw_flow_file'], region_name=c['city_name'], time=8, sources=True)
+W, t, sources, ends = region_grid.create_flow_matrix(c['raw_flow_file'], region_name=c['city_name'], time=8,
+                                                     sources=True)
 
 size = region_grid.grid_size * region_grid.grid_size
 
+time_sequences = random_walk_time(t, source_v=sources, walk_length=walk_length,
+                                  n_walks=n_walks)
 distance_sequences = random_walk_distance(distance_matrix, high=size, walk_length=walk_length, n_walks=n_walks)
-time_sequences = random_walk_time(t, source_v=sources, walk_length=walk_length, n_walks=n_walks)
 
 print("Sequences Created")
+print(sources)
+print(ends)
 sequences = []
 for r in region_grid.matrix_idx_map.values():
     # add identity sequence if the region is never seen
     if r not in sources.union(ends):
-        sequences.append(" ".join([idx for idx in str(r) * walk_length]))
+        sequences.append((f"{str(r)} " * walk_length).split(" "))
 
 for seq in distance_sequences + time_sequences:
-    s = " ".join([str(id) for id in seq])
-    print(s + "\n")
+    s = [str(id) for id in seq]
+    # print(list(s) + "\n")
     sequences.append(s)
-
 print(f"Sequence Count {len(sequences)}")
 # each sequence is the walk from region idx to the other
 
@@ -102,7 +111,7 @@ model = gensim.models.Word2Vec(
     sequences,
     size=20,  # adjustable
     window=8,  # walk length
-    min_count=2,
+    min_count=1,
     negative=5,
     workers=8  # number of cores on machine
 )
